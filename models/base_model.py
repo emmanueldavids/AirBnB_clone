51ded74bb0daf2e3f8ec6fe239719f9b333d1746
emmanueldavids <emmanueldavids417@gmail.com>
2023-03-13 06:56:24 +0000

    model file

--- models/base_model.py
+++ models/base_model.py
@@ -6,19 +6,23 @@ import datetime
 
 
 class BaseModel:
+    # Initializing a method for the base class
     def __init__(self, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
     
     def __str__(self):
+        # converts the BaseModel into a string
         cls_name = type(self).__name__
         return f"[{cls_name}]{self.id} {self.created_at}]"
     
     def save(self):
+        # saves the updated datetime
         self.updated_at = datetime.datetime.now()
         
     def to_dict(self):
+        # converting the attributes to a dictionary
         base_dict = self.__dict__
         base_dict['__class__'] = self.__class__.__name__
         base_dict['created_at'] = self.created_at
